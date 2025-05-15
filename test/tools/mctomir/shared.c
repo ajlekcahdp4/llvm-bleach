f95d2dfaac0fa1576a2920ec932695bba7fe25c2
Alexander Romanov <alex.rom23@mail.ru>
2025-05-15 22:28:39 +0300

    test(mctomir): add shared library lifting test

---
+++ test/tools/mctomir/shared.c
@@ -0,0 +1,24 @@
+// RUN: gcc -fPIC -c %s -o %t.o
+// RUN: gcc -shared %t.o -o %t.so
+// RUN: %bin/mctomir %t.so -o - | FileCheck %s
+
+int foo(int);
+
+int bar(int x) { return foo(x - 1); }
+
+int foo(int x) {
+  if (x == 1)
+    return 1;
+  return x * bar(x);
+}
+
+// CHECK: name: bar
+// CHECK: body:
+// CHECK: bb.0:
+// CHECK: CALL
+// CHECK: RET64
+// CHECK: name: foo
+// CHECK: body:
+// CHECK: bb.0:
+// CHECK: CALL
+// CHECK: RET64
